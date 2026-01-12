c143ecfdc11aa897f3ac34cd0a2797df257cb29d
alifaizyab8 <alifaizyab8@gmail.com>
2026-01-12 20:37:55 +0500

    improved formatting

--- Lab1/__main.cpp
+++ Lab1/__main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
+
 class student
 {
     string name;
@@ -9,57 +9,32 @@ class student
     int quiz[3];
 
 public:
-    // Setters for individual properties
     void setName()
-    {
-        cout << "Enter Student name : " << endl;
-        cin >> name;
-    }
+    {   cout << "Enter Student name: ";
+        cin >> name; }
     void setRoll()
-    {
-        cout << "Enter Student Roll Number : " << endl;
-        cin >> roll;
-    }
+    {   cout << "Enter Student Roll Number: ";
+        cin >> roll; }
     void setQuiz()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            cout << "Quiz " << i + 1 << ": " << endl;
-
+    {   for (int i = 0; i < 3; i++) {
+            cout << "Quiz " << i + 1 << ": ";
             cin >> quiz[i];
-        }
-    }
-    // Getters for Individual Properties
-    void getName()
-    {
-        cout << "Student name: " << name << endl;
-    }
-    void getRoll()
-    {
-        cout << "Roll number: " << roll << endl;
-    }
+        } }
+    void getName() { cout << "Student name: " << name << endl; }
+    void getRoll() { cout << "Roll number: " << roll << endl; }
     void getQuiz()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            cout << "Quiz " << i + 1 << ": " << quiz[i] << endl;
-        }
-    }
-    // Printing Function
+    {   for (int i = 0; i < 3; i++)
+            cout << "Quiz " << i + 1 << ": " << quiz[i] << endl; }
     void printStudent()
-    {
-        getName();
+    {   getName();
         getRoll();
-        getQuiz();
-    }
+        getQuiz(); }
 };
 
 int main()
-{
-    student s1;
+{   student s1;
     s1.setName();
     s1.setRoll();
     s1.setQuiz();
     s1.printStudent();
-    return 0;
-}
+    return 0; }
--- Lab1/__main1.cpp
+++ Lab1/__main1.cpp
@@ -2,8 +2,7 @@
 #include <string>
 using namespace std;
 
-class student
-{
+class student {
     string name;
     int roll;
     int quiz[3];
@@ -18,58 +17,25 @@ public:
     void printStudent();
 };
 
-void student::setName()
-{
-    cout << "Enter Student name : " << endl;
-    cin >> name;
-}
-
-void student::setRoll()
-{
-    cout << "Enter Student Roll Number : " << endl;
-    cin >> roll;
-}
-
+void student::setName() { cin >> name; }
+void student::setRoll() { cin >> roll; }
 void student::setQuiz()
-{
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "Quiz " << i + 1 << ": " << endl;
-        cin >> quiz[i];
-    }
-}
-
-void student::getName()
-{
-    cout << "Student name: " << name << endl;
-}
-
-void student::getRoll()
-{
-    cout << "Roll number: " << roll << endl;
-}
-
+{   for (int i = 0; i < 3; i++)
+        cin >> quiz[i]; }
+void student::getName() { cout << "Student name: " << name << endl; }
+void student::getRoll() { cout << "Roll number: " << roll << endl; }
 void student::getQuiz()
-{
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "Quiz " << i + 1 << ": " << quiz[i] << endl;
-    }
-}
-
+{   for (int i = 0; i < 3; i++)
+        cout << "Quiz " << i + 1 << ": " << quiz[i] << endl; }
 void student::printStudent()
-{
-    getName();
+{   getName();
     getRoll();
-    getQuiz();
-}
+    getQuiz(); }
 
 int main()
-{
-    student s1;
+{   student s1;
     s1.setName();
     s1.setRoll();
     s1.setQuiz();
     s1.printStudent();
-    return 0;
-}
+    return 0; }

6c555ef0bfa8f217e15127bc3dd7a5d47318b1a0
alifaizyab8 <alifaizyab8@gmail.com>
2026-01-12 18:43:02 +0500

    Completed Q1

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 using namespace std;
+class complex_number
+{
+    double real, img;
 
-int main() {
+public:
+    // Setters for real and imaginary values
+    void set_real()
+    {
+        cout << "Please enter the value of Real Part" << endl;
+        cin >> real;
+    }
+    void set_img()
+    {
+        cout << "Please enter the value of Imaginary Part" << endl;
+        cin >> img;
+    }
+    void print()
+    {
+        if (img > 0)
+        {
+            cout << real << " + " << img << "i" << endl;
+        }
+        else
+        {
+            cout << real << " - " << (-1.0) * img << "i" << endl;
+        }
+    }
+};
+int main()
+{
+    complex_number c1;
+    c1.set_real();
+    c1.set_img();
+    c1.print();
+
     return 0;
 }
---
+++ Lab1/main1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class complex_number
+{
+    double real, img;
+
+public:
+    // Setters for real and imaginary values
+    void set_real();
+    void set_img();
+    void print();
+};
+void complex_number ::set_real()
+{
+    cout << "Please enter the value of Real Part" << endl;
+    cin >> real;
+}
+void complex_number ::set_img()
+{
+    cout << "Please enter the value of Imaginary Part" << endl;
+    cin >> img;
+}
+void complex_number ::print()
+{
+    if (img > 0)
+    {
+        cout << real << " + " << img << "i" << endl;
+    }
+    else
+    {
+        cout << real << " - " << (-1.0) * img << "i" << endl;
+    }
+}
+int main()
+{
+    complex_number c1;
+    c1.set_real();
+    c1.set_img();
+    c1.print();
+
+    return 0;
+}
